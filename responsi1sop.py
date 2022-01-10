a8d9398da643a69bc577f0595d5bebf387fd7d1a
Gressensia <gressensiaoliviana@gmail.com>
2022-01-10 19:10:25 +0700

    create responsi1sop.py

--- responsi1sop.py
+++ responsi1sop.py
@@ -1,1 +1,29 @@
+import helper
+
+#5200411122
+#Gressensia Olivia Neno Aumenu
+helper.header("Responsi NO.1")
+
+Ram = int(input("Kapasitas RAM : " ))
+totalBlok = int(input("Blok/unit : "))
+petabit = helper.hitungPetaBit(helper.ubahRamKeMbps(Ram), totalBlok)
+kapasitas = petabit - Ram
+
+print("Total peta bit : ", petabit , "KB " )
+print("Kapasitas per Petabit : " , kapasitas)
+print("\nProgram tereksekusi")
+
+print("-"*40)
+ramUntukSOP = int(input("Kapasitas Sistem Operasi  : "))
+program1 = int(input("RAM yang digunakan Program 1 : "))
+program2 = int(input("RAM yang digunakan Program 2 : "))
+
+totalRam = program1 + program2
+totalRAMygtidakterpakai = Ram - totalRam
+
+print("Total RAM yang Terpakai = ", totalRam)
+print("Total RAM yang tidak Terpakai = ", totalRAMygtidakterpakai)
+
+print("\njumlah blok bernilai 1 = ", totalRam)
+print("jumlah blok bernilai 0 = ", totalBlok - totalRam)
 
