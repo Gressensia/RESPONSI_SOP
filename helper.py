c3edee8eb6edbb437d914221ac7a3e69166e573e
Gressensia <gressensiaoliviana@gmail.com>
2022-01-10 19:07:21 +0700

    create helper.py

--- helper.py
+++ helper.py
@@ -1,1 +1,10 @@
+def header(title):
+    print("-" * 45)
+    print("\t\t", title)
+    print("-" * 45)
 
+def ubahRamKeMbps(ramInGbps):
+    return ramInGbps * 1024
+
+def hitungPetaBit(ram, blok):
+    return ram / blok
